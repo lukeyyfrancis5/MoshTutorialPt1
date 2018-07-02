2ae16268ca4dd01e9377984dd3182ab41624d00e
lukebrandonfrancis <lukebrandonfrancis@gmail.com>
2018-07-02 01:50:23 -0400

    Add project files.

---
+++ MoshTutorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoshTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
